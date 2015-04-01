7059817e88bc32cb6197aaad4dcfedf0cc49bbbd
sebastian90 <sebastian90@localhost>
2015-04-01 10:28:26 +0200

    Using ABS in NormalGenerator

--- src/main/java/it/unica/tcs/NormalGenerator.java
+++ src/main/java/it/unica/tcs/NormalGenerator.java
@@ -2,22 +2,23 @@ package it.unica.tcs;
 
 import cern.jet.random.Normal;
 import cern.jet.random.engine.RandomEngine;
+import java.lang.Math;
 
 /**Generates normal distributed random integers  */
 public class NormalGenerator {
+	
 	private RandomEngine randomEngine;
 	private double standardDeviation;
 	
-	/** Instantiates a random numbers generator object 
-	 * 
+	/** Instantiates a random numbers generator object  
 	 * @param sd standard deviation of the normal distribution
 	 */
-	public NormalGenerator(double sd){
+	public NormalGenerator(double sd) {
+		
 		randomEngine = RandomEngine.makeDefault();
 		this.standardDeviation = sd;
 	}
 	/**Generates a number in the range [0, limit) (limit not included).
-	 * 
 	 * @param limit 
 	 * @return integer value
 	 */
@@ -29,16 +30,22 @@ public class NormalGenerator {
 		double standardDeviation = this.standardDeviation;
 		
 		double correctionFactor = Math.log10(limit);
-		if(correctionFactor > 1){
+		
+		if (correctionFactor > 1) {
+			
 		    standardDeviation = standardDeviation / correctionFactor;
 		}
 		
 		Normal normalGen = new Normal(0, standardDeviation, randomEngine);;
 		
-		double n=-1;		
-		while(n<0 || n>=limit){
-			n = normalGen.nextDouble()*limit;
-			Log.message().fine("Extracted: " + n);
+		double n = -1;
+		
+		while (n<0 || n>=limit) {
+			
+			n = (normalGen.nextDouble() * limit);
+			n = Math.abs(n); // Takes always the absolute
+		
+			// Log.message().fine("Extracted: " + n);
 		}
 		
 		return (int) Math.floor(n);		
