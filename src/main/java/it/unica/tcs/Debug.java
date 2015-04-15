2a231159cf0c7d7dd893a949dae9eea4ca9b903b
Sebastian Podda <sebastianpodda@gmail.com>
2015-04-15 22:14:51 +0200

    New Debug class with query for DB populating

---
+++ src/main/java/it/unica/tcs/Debug.java
@@ -0,0 +1,71 @@
+package it.unica.tcs;
+
+import java.sql.SQLException;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/debug")
+public class Debug {
+
+	@POST
+	@Path("/tellStandard")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ResponsePacket tellStandard(QueryPacket postData) {
+		
+		DatabaseInterface db = MainApplication.getDBConnection();
+    	
+    	try {
+			db.deleteContracts();
+			
+		} catch (SQLException e) {
+			
+			Log.message().severe("Database cleaning failed");
+			return new ResponsePacket(-1, "Cannot delete anything from the DB.");
+		}
+		
+		Double N = Double.valueOf(postData.getFirstContract());
+		Double K = Double.valueOf(postData.getSecondContract());
+		
+		Log.message().warning("Populating the DB with N=" + N + " and K=" + K + ".");
+		
+        String[] input = new String[1];
+        String path;
+        path = Tools.PATH_CTU + Tools.CTU_PARAM_TRANSLATE;
+        
+        input[0] = "?pay{;t}.(?ok & ?dispute{t<10; t}.!refund{t<7} & ?abort)"; // compliant
+		
+		String firstXml = Tools.callApplication(path, input, false);
+        
+        input[0] = firstXml + "\n";
+        String firstMapping = Tools.callApplication(Tools.PATH_CTU + Tools.CTU_PARAM_BUILD_AUTOMATON, input, false);
+        String firstLabels = Tools.callApplication(Tools.PATH_CTU + Tools.CTU_PARAM_GET_LABELS, input, false);
+        String firstHash = Tools.hashContract(firstXml, MainApplication.getRand());
+        
+        input[0] = "?pay{;t}.(?ok & ?dispute{t<9; t}.!refund{t<7} & ?abort)"; // not compliant
+		
+		String secondXml = Tools.callApplication(path, input, false);
+        
+        input[0] = firstXml + "\n";
+        String secondMapping = Tools.callApplication(Tools.PATH_CTU + Tools.CTU_PARAM_BUILD_AUTOMATON, input, false);
+        String secondLabels = Tools.callApplication(Tools.PATH_CTU + Tools.CTU_PARAM_GET_LABELS, input, false);
+        String secondHash = Tools.hashContract(firstXml, MainApplication.getRand());
+        
+        try {
+	        for (int i=0; i<N; i++) {
+	        	
+	        	if (Math.random() < K/N) 
+	        		db.insertContract(firstHash, firstXml, 1, 0, DatabaseInterface.CONTRACT_ROLE_LATENT, DatabaseInterface.CONTRACT_LATENT, new Long(MainApplication.getRand()), ComplianceChecker.getContractType(firstXml), firstMapping, firstLabels); 
+	        	else
+	        		db.insertContract(secondHash, secondXml, 1, 0, DatabaseInterface.CONTRACT_ROLE_LATENT, DatabaseInterface.CONTRACT_LATENT, new Long(MainApplication.getRand()), ComplianceChecker.getContractType(secondXml), secondMapping, secondLabels);
+	        }
+        }
+        catch (SQLException s) {
+        	
+        	Log.message().severe("SQL error when populating the DB: " + s.getMessage());
+        }
+		return new ResponsePacket(1, "Done!");
+	}
+}
